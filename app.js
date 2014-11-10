52f0186b0e12f43554a46e1cedf71abfcb7c52d0
Scott Prue <prescottprue@gmail.com>
2014-11-10 03:38:55 -0500

    [TEST] Trying app.all

--- app.js
+++ app.js
@@ -31,16 +31,16 @@ app.use('/auth', auth);
 // API HEADERS
 
 // Add headers
-app.use(function (req, res, next) {
+app.all( '*', function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
