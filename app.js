0a10b0ac948f9952046118dc8677e896f9fe4dce
Scott Prue <prescottprue@gmail.com>
2014-11-10 03:33:19 -0500

    Access-Control-Origin switched to origin variable.

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use('/auth', auth);
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
