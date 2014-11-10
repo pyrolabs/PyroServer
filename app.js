a64c667953c5eb2d61160c4b04a955cc2f729001
Scott Prue <prescottprue@gmail.com>
2014-11-10 03:45:53 -0500

    [TEST] CORS package.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var auth = require('./routes/auth');
@@ -29,26 +29,26 @@ app.use('/auth', auth);
 
 
 // API HEADERS
-
+app.use(cors());
 // Add headers
-app.all( '*', function (req, res, next) {
+// app.all( '*', function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.header('Access-Control-Allow-Origin', '*');
+//     // Website you wish to allow to connect
+//     res.header('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     // Request headers you wish to allow
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     // res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 
 
